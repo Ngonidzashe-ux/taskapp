20231bf480a77858bd2517fee2a3ac88d6f56ed9
Ngonidzashe Maposa <ngonimaposss@gmail.com>
2026-06-08 08:56:08 +0800

    charisma huh

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 int main(){
 
-std::cout << "My name is main. I was written by a Zimbabwean Christian Man of Good Height. He also has good charisma. He is a hockey player" << std::endl;
+std::cout << "My name is main. I was written by a Zimbabwean Christian Man of Good Height. He also has good charisma. He is a hockey player. He is a gee. He is from Zvishtown" << std::endl; 
 
 return 0;
 
