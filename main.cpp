bc8f2a7e84d40182425d05e466629fe2f5064044
Ngonidzashe Maposa <ngonimaposss@gmail.com>
2026-06-08 08:41:04 +0800

    Change main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 int main(){
 
-std::cout << "My name is main. I was written by a Zimbabwean Christian Man of Good Height" << std::endl;
+std::cout << "My name is main. I was written by a Zimbabwean Christian Man of Good Height. He also has good charisma. He is a hockey player" << std::endl;
 
 return 0;
 
